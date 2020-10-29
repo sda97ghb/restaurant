de081eac45d79df4d57a2604efe7f9e9c596a9a2
sda97g <sda97g@gmail.com>
2020-10-29 23:39:20 +0500

    Add SQL logging when DEBUG=True

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -137,3 +137,29 @@ CURRENCY_CHOICES = [
     ("EUR", "EUR €"),
     ("USD", "USD $"),
 ]
+
+
+# Logging
+# https://www.neilwithdata.com/django-sql-logging
+
+LOGGING = {
+    'version': 1,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
+}
